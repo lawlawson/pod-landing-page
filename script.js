11fac51a8fd00a17bfba6f5ead87ea5fd42b7945
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2023-03-07 22:07:13 +0000

    updated js script

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// function validateEmail(emailId) {
-//   const mailformat = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-
-//   if(emailId.value.match(mailformat)) {
-//     document.form1.text1.focus();
-//     return true;
-//   } else {
-//     alert('Invalid email address');
-//     document.form1.text1.focus();
-//     return false;
-//   }
-// }
-
 const form = document.querySelector('form');
 const input = document.querySelector('.email_address');
 const spanErr = document.querySelector('.error_message');
